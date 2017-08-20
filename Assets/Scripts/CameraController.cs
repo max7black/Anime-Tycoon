15e6b64d35b77d3dcda3fc581f49fbe958f41f5c
Cole Arnold <max7black@gmail.com>
2017-08-20 15:50:40 -0500

    Added tempoary 3d character
    Added UnityChan asset to explore option of possible character design and
    to have a generic character model to use.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera1Controller : MonoBehaviour
+{
+    public float speed = 0.1F;
+
+    void Update()
+    {
+        if ((Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved))
+        {
+            Vector2 touchDeltaPosition = Input.GetTouch(0).deltaPosition;
+            transform.Translate(-touchDeltaPosition.x * speed, -touchDeltaPosition.y * speed, 0);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(new Vector3(-speed * Time.deltaTime, 0, 0));
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(new Vector3(0, -speed * Time.deltaTime, 0));
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(new Vector3(0, speed * Time.deltaTime, 0));
+        }
+    }
+}
--- Assets/Scripts/GenreMananger.cs
+++ Assets/Scripts/GenreMananger.cs
@@ -4,12 +4,21 @@ using UnityEngine;
 
 public class GenreMananger : MonoBehaviour {
 
+    string actionLevel = "actionLevel";
+    string adventureLevel = "adventureLevel";
+    string romanceLevel = "romanceLevel";
+    string comedyLevel = "comedyLevel";
 
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+
+    // Use this for initialization
+    void Start () {
+
+        PlayerPrefs.SetInt("actionLevel", 1);
+        PlayerPrefs.SetInt("adventureLevel", 1);
+        PlayerPrefs.SetInt("romanceLevel", 1);
+        PlayerPrefs.SetInt("comedyLevel", 1);
+    }
 	
 	// Update is called once per frame
 	void Update () {
